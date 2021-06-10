76eab611c66abc986ea88c89f4977f1c00f3eb5b
Ed Jefferson <e.jefferson@gmail.com>
2021-06-10 19:47:16 +0100

    egg

--- express/server.js
+++ express/server.js
@@ -4,12 +4,52 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const HTMLParser = require('node-html-parser');
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+
+
+
+router.get('/', (req, resmain) => {
+  resmain.writeHead(200, { 'Content-Type': 'text/html' });
+  
+
+  const https = require('https')
+  var url = "https://store.steampowered.com/app/1290000/PowerWash_Simulator/"
+  https.get(url, res => {
+    let data = '';
+    res.on('data', chunk => {
+      data += chunk;
+    });
+    res.on('end', () => {
+      var similar_text = data.split("{\"rgApps\":")[1].split(",\"rgPackages")[0]
+
+      
+      var root = HTMLParser.parse(data);
+      console.log(root.querySelector('.game_review_summary').rawText)
+      console.log(root.querySelector('.release_date').rawText)
+      console.log(root.querySelector('#developerList').rawText)
+      console.log(root.querySelector('#publisherList').rawText)
+
+      var similar_games =  similar_text.split("},\"").map(
+        x =>  JSON.parse(
+          "{" + x.split(':{').slice(1).join(':{').replace("}}","") +  "}"
+          
+          )
+      )
+      resmain.write(
+        
+        JSON.stringify(similar_games)
+      
+      )
+      resmain.end();
+
+
+    })
+  }).on('error', err => {
+    console.log(err.message);
+  }).end()
+
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
