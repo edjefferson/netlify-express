d4d1f5aaca68fc4b803b84a0626a06bbb18324a8
Ed Jefferson <e.jefferson@gmail.com>
2021-06-10 20:10:21 +0100

    egg

--- express/server.js
+++ express/server.js
@@ -6,42 +6,50 @@ const app = express();
 const bodyParser = require('body-parser');
 const HTMLParser = require('node-html-parser');
 
-const router = express.Router();
 
 
 
-router.get('/', (req, resmain) => {
+app.get('/', (req, resmain) => {
   resmain.writeHead(200, { 'Content-Type': 'text/html' });
-  
+  const steam_id = req.query.steamId
+  console.log("egg")
 
+  console.log(req.query.steamId)
   const https = require('https')
-  var url = "https://store.steampowered.com/app/1290000/PowerWash_Simulator/"
+  var url = "https://store.steampowered.com/app/"+steam_id+"/PowerWash_Simulator/"
   https.get(url, res => {
     let data = '';
     res.on('data', chunk => {
       data += chunk;
     });
     res.on('end', () => {
-      var similar_text = data.split("{\"rgApps\":")[1].split(",\"rgPackages")[0]
+      
+      try {
+        var similar_text = data.split("{\"rgApps\":")[1].split(",\"rgPackages")[0]
 
       
       var root = HTMLParser.parse(data);
-      console.log(root.querySelector('.game_review_summary').rawText)
-      console.log(root.querySelector('.release_date').rawText)
-      console.log(root.querySelector('#developerList').rawText)
-      console.log(root.querySelector('#publisherList').rawText)
-
-      var similar_games =  similar_text.split("},\"").map(
-        x =>  JSON.parse(
-          "{" + x.split(':{').slice(1).join(':{').replace("}}","") +  "}"
+      console.log(root.querySelector('.game_review_summary').rawText.trim())
+      console.log(root.querySelector('.release_date').rawText.trim())
+      console.log(root.querySelector('#developerList').rawText.trim())
+      console.log(root.querySelector('#publisherList').rawText.trim())
+        var similar_games =  similar_text.split("},\"").map(
+          x =>  JSON.parse(
+            "{" + x.split(':{').slice(1).join(':{').replace("}}","") +  "}"
+            
+            )
+        )
+        resmain.write(
           
-          )
-      )
-      resmain.write(
+          JSON.stringify(similar_games)
         
-        JSON.stringify(similar_games)
+        )
       
-      )
+      } catch(e) {
+        resmain.write(e.message);
+
+      }
+
       resmain.end();
 
 
@@ -51,12 +59,7 @@ router.get('/', (req, resmain) => {
   }).end()
 
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
