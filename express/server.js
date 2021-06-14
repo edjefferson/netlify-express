bca25e1eab45ba5d3f41091d559aab6b777c5e9b
Ed Jefferson <e.jefferson@gmail.com>
2021-06-14 11:02:13 +0100

    basic version

--- express/server.js
+++ express/server.js
@@ -5,66 +5,104 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const HTMLParser = require('node-html-parser');
+const https = require('https')
+const got = require('got');
+
 
 const router = express.Router();
 
+const getPlayerCount = async(id) => {
 
+      try {
+        let url = "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=" + id + "&key=702C5A7D15512AE0477A5D60DC28A724"
+
+         const response = await got(url, { json: true })
+        return response.body.response.player_count
+      } catch (error) {
+        console.log(error.response.body);
+      }
+  }
+
+  
 
 router.post('/', (req, resmain) => {
-  var id = req.body.id
 
-  console.log(req.body.id)
 
-  const https = require('https')
-  var url = "https://store.steampowered.com/app/"+id+"/PowerWash_Simulator/"
-  https.get(url, res => {
-    let data = '';
-    res.on('data', chunk => {
-      data += chunk;
-    });
-    res.on('end', () => {
+  (async () => {
+    let id = req.body.id
+
+    let url = "https://store.steampowered.com/app/"+id+"/PowerWash_Simulator/"
+
+    const res = await got(url, { json: false });
+    const data = res.body
+
+        
+    try {
+      const similar_text = data.split("{\"rgApps\":")[1].split(",\"rgPackages")[0]
+
+    
+      const root = HTMLParser.parse(data);
+      const name = root.querySelector('#appHubAppName').rawText.trim()
+
+        
+      const review_summary = root.querySelector('.game_review_summary').rawText.trim()
+      const release_date = root.querySelector('.release_date').querySelector('.date').rawText.trim()
+      const developers = root.querySelector('#developerList').querySelectorAll('a').map(x => x.rawText.trim())
+      const publishers = root.querySelector('#publisherList').querySelectorAll('a').map(x => x.rawText.trim())
+      const languages = root.querySelector('.game_language_options').querySelectorAll("tr").slice(1)
+        .map( row => row.querySelector('td').rawText.trim())
+      const tags =  JSON.parse("[{" + data.split("InitAppTagModal")[1].split("[{")[1].split("}],")[0] + "}]").map(x => x.name)
       
-      try {
-        var similar_text = data.split("{\"rgApps\":")[1].split(",\"rgPackages")[0]
+      const genres = root.querySelector('.details_block').querySelectorAll('a[href*="genre"]').map(x => x.rawText.trim())
 
-      
-      var root = HTMLParser.parse(data);
-      console.log(root.querySelector('.game_review_summary').rawText)
-      console.log(root.querySelector('.release_date').rawText)
-      console.log(root.querySelector('#developerList').rawText)
-      console.log(root.querySelector('#publisherList').rawText)
-        var similar_games =  similar_text.split("},\"").map(
-          x =>  JSON.parse(
-            "{" + x.split(':{').slice(1).join(':{').replace("}}","") +  "}"
-            
-            )
-        )
-
-        similar_games.map (x => {
-          x["id"] = x["small_capsulev5"].split("/")[5]
-          return x
-        })
-        console.log(req.headers.referer) 
-        resmain.writeHead(200, { 'Content-Type': 'application/json' });
-
-        resmain.write(
-          JSON.stringify(similar_games.slice(0, 9))
+      let similar_games =  similar_text.split("},\"").map(
+        x =>  
+          JSON.parse(
+          "{" + x.split(':{').slice(1).join(':{').replace("}}","") +  "}"
           )
-      
-      } catch(e) {
-        resmain.write(e.message);
+        
+      )
 
+      similar_games.map (x => {
+        x["id"] = x["small_capsulev5"].split("/")[5]
+        return x
+      })
+      resmain.writeHead(200, { 'Content-Type': 'application/json' });
+      let playerCount
+      await getPlayerCount(id).then(x=> playerCount = x)
+      const payload = {
+        similarGames: similar_games.slice(0, 9),
+        id: id,
+        name: name,
+        reviewSummary: review_summary,
+        releaseDate: release_date,
+        developers: developers.join(", "),
+        publishers: publishers.join(", "),
+        languages: languages.join(", "),
+        tags: tags.join(", "),
+        genres: genres.join(", "),
+        playerCount: playerCount
       }
+  //        console.log(getSteamDb(id))
 
-      resmain.end();
+      resmain.write(
+        JSON.stringify(payload)
+      )
+    } catch(e) {
+      resmain.write(e.message);
+      console.log("error")
+      console.log(e.message)
+      console.log(e.stack)
 
 
-    })
-  }).on('error', err => {
-    console.log(err.message);
-  }).end()
+    }
 
-});
+    resmain.end();
+
+
+  })();
+})
+
 
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 
