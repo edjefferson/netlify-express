c6c4d7a193d1cd36450ce2fb8e791c1e80fcb4e5
Ed Jefferson <e.jefferson@gmail.com>
2021-07-09 10:38:27 +0100

    fixes

--- express/server.js
+++ express/server.js
@@ -65,136 +65,138 @@ const getSteamSpy = async(id) => {
   }
 }
 
-  
+let retries = 0
+
 
 router.post('/', (req, resmain) => {
 
-
   (async () => {
+    let headersent = false
+
     let id = req.body.id
 
-    let url = "https://store.steampowered.com/app/"+id+"/PowerWash_Simulator/"
-
-    const res = await got(url, { json: false });
-    const data = res.body
-
-        
-    try {
-      let similar_games
+    while (retries < 3) {
       try {
+        let url = "https://store.steampowered.com/app/"+id+"/PowerWash_Simulator/"
+
+        const res = await got(url, { json: false });
+        const data = res.body
+        
         const similar_text = data.split("{\"rgApps\":")[1].split(",\"rgPackages")[0]
-        similar_games =  similar_text.split("},\"").map(
+        let similar_games =  similar_text.split("},\"").map(
           x =>  
             JSON.parse(
             "{" + x.split(':{').slice(1).join(':{').replace("}}","") +  "}"
             )
           
         )
-  
+
         similar_games.map (x => {
           x["id"] = x["small_capsulev5"].split("/")[5]
           return x
         })
-      }
-      catch(error) {
-        console.log(error)
-      }
+        
+        
 
-      const root = HTMLParser.parse(data);
-      const name = root.querySelector('#appHubAppName').rawText.trim()
-      console.log(id)
-      console.log(name)
+        const root = HTMLParser.parse(data);
+        const name = root.querySelector('#appHubAppName').rawText.trim()
+        console.log(id)
+        console.log(name)
 
-      const review_summary = root.querySelector('.game_review_summary').rawText.trim()
-      const release_date = root.querySelector('.release_date').querySelector('.date').rawText.trim()
-      const developers = root.querySelectorAll('.dev_row')[0].querySelectorAll('a').map(x => x.rawText.trim())
-      const publishers = root.querySelectorAll('.dev_row')[1].querySelectorAll('a').map(x => x.rawText.trim())
-      const languages = root.querySelector('.game_language_options').querySelectorAll("tr").slice(1)
-        .map( row => row.querySelector('td').rawText.trim())
-      const tags =  JSON.parse("[{" + data.split("InitAppTagModal")[1].split("[{")[1].split("}],")[0] + "}]").map(x => x.name)
-      
-      const genres = root.querySelector('.details_block').querySelectorAll('a[href*="genre"]').map(x => x.rawText.trim())
-      
+        const review_summary = root.querySelector('.game_review_summary').rawText.trim()
+        const release_date = root.querySelector('.release_date').querySelector('.date').rawText.trim()
+        const developers = root.querySelectorAll('.dev_row')[0].querySelectorAll('a').map(x => x.rawText.trim())
+        const publishers = root.querySelectorAll('.dev_row')[1].querySelectorAll('a').map(x => x.rawText.trim())
+        const languages = root.querySelector('.game_language_options').querySelectorAll("tr").slice(1)
+          .map( row => row.querySelector('td').rawText.trim())
+        const tags =  JSON.parse("[{" + data.split("InitAppTagModal")[1].split("[{")[1].split("}],")[0] + "}]").map(x => x.name)
+        
+        const genres = root.querySelector('.details_block').querySelectorAll('a[href*="genre"]').map(x => x.rawText.trim())
+        
 
-      let reviews = ["review_type_all","review_type_positive","review_type_negative"].map(r => {
-          if (root.querySelector("label[for=\""+r+"\"]").querySelector('span')) {
-            return root.querySelector("label[for=\""+r+"\"]").querySelector('span').rawText.trim().slice(1).slice(0, -1).replace(",", '' )
-          } else {
-            return undefined
+        let reviews = ["review_type_all","review_type_positive","review_type_negative"].map(r => {
+            if (root.querySelector("label[for=\""+r+"\"]").querySelector('span')) {
+              return root.querySelector("label[for=\""+r+"\"]").querySelector('span').rawText.trim().slice(1).slice(0, -1).replace(",", '' )
+            } else {
+              return undefined
+            }
           }
+        )
+        let metascore = ""
+        
+        if (root.querySelector('#game_area_metascore')) metascore = root.querySelector('#game_area_metascore').querySelector('.score').rawText.trim()
+
+        let purchaseActions = root.querySelectorAll('.game_purchase_action_bg')
+        let purchaseAction = purchaseActions.filter( p => p.querySelector(".discount_original_price") || p.querySelector(".game_purchase_price"))[0]
+
+        let discountedPrice 
+        let originalPrice
+
+        if (purchaseAction.querySelector(".discount_original_price")) {
+          discountedPrice = purchaseAction.querySelector(".discount_final_price").rawText.trim()
+          originalPrice = purchaseAction.querySelector(".discount_original_price").rawText.trim()
         }
-      )
-      let metascore = ""
+        else {
+          discountedPrice = purchaseAction.querySelector(".game_purchase_price").rawText.trim()
+          originalPrice = purchaseAction.querySelector(".game_purchase_price").rawText.trim()
+        }
+        
+        
+
+        let playerCount
+        await getPlayerCount(id).then(x=> playerCount = x)
+
+
+
       
-      if (root.querySelector('#game_area_metascore')) metascore = root.querySelector('#game_area_metascore').querySelector('.score').rawText.trim()
+        
+        let payload = {
+          similarGames: similar_games.slice(0, 9),
+          id: id,
+          name: name,
+          releaseDate: release_date.replace(",",""),
+          discountedPrice: discountedPrice,
+          originalPrice: originalPrice,
+          developers: developers.join(";"),
+          publishers: publishers.join(";"),
+          languages: languages.join(";"),
+          tags: tags.join(";"),
+          genres: genres.join(";"),
+          playerCount: playerCount,
+          reviewSummary: review_summary,
 
-      let purchaseActions = root.querySelectorAll('.game_purchase_action_bg')
-      let purchaseAction = purchaseActions.filter( p => p.querySelector(".discount_original_price") || p.querySelector(".game_purchase_price"))[0]
+          allReviews: reviews[0],
+          positiveReviews: reviews[1],
+          negativeReviews: reviews[2],
+          metascore: metascore
 
-      let discountedPrice 
-      let originalPrice
+        }
+    //        console.log(getSteamDb(id))
+        await getSteamSpy(id).then(x=> {
 
-      if (purchaseAction.querySelector(".discount_original_price")) {
-        discountedPrice = purchaseAction.querySelector(".discount_final_price").rawText.trim()
-        originalPrice = purchaseAction.querySelector(".discount_original_price").rawText.trim()
-      }
-      else {
-        discountedPrice = purchaseAction.querySelector(".game_purchase_price").rawText.trim()
-        originalPrice = purchaseAction.querySelector(".game_purchase_price").rawText.trim()
-      }
-      
-      
-
-      let playerCount
-      await getPlayerCount(id).then(x=> playerCount = x)
-
-
-
-    
-      
-      let payload = {
-        similarGames: similar_games.slice(0, 9),
-        id: id,
-        name: name,
-        releaseDate: release_date.replace(",",""),
-        discountedPrice: discountedPrice,
-        originalPrice: originalPrice,
-        developers: developers.join(";"),
-        publishers: publishers.join(";"),
-        languages: languages.join(";"),
-        tags: tags.join(";"),
-        genres: genres.join(";"),
-        playerCount: playerCount,
-        reviewSummary: review_summary,
-
-        allReviews: reviews[0],
-        positiveReviews: reviews[1],
-        negativeReviews: reviews[2],
-        metascore: metascore
-
-      }
-  //        console.log(getSteamDb(id))
-      await getSteamSpy(id).then(x=> {
-
-        Object.keys(x).forEach( (d, i) => {
-          payload[d] = x[d]
+          Object.keys(x).forEach( (d, i) => {
+            payload[d] = x[d]
+          })
         })
-      })
+        
+        if (headersent === false) {
+          resmain.writeHead(200, { 'Content-Type': 'application/json' })
+          headersent = true
+        }
+        resmain.write(
+          JSON.stringify(payload)
+        )
+        resmain.end();
+      } catch(e) {
+        console.log("error")
+        console.log(e.message)
+        console.log(e.stack)
+        retries += 1
 
-      resmain.writeHead(200, { 'Content-Type': 'application/json' });
-      resmain.write(
-        JSON.stringify(payload)
-      )
-    } catch(e) {
-      resmain.write(e.message);
-      console.log("error")
-      console.log(e.message)
-      console.log(e.stack)
-
-
+      }
     }
 
-    resmain.end();
+    
 
 
   })();
